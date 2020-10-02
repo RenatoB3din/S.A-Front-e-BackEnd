e2fae5f8d96f6068bc52bbfc3cf4070647ff65e0
RenatoB3din <emailrenatob3din@gmail.com>
2020-10-01 22:30:33 -0300

    Tela de Relatorio

---
+++ frontend-master/src/components/Layout/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Card.css";
+
+export default props => {
+
+    const cardStyle = {
+        backgroundColor: props.color || '#F00',
+        borderColor: props.color || '#F00'
+    }
+
+    return (
+        <div className="Card" style={cardStyle}>
+            <div className="Title"><strong>{props.titulo}</strong></div>
+            <div className="Url"> <img src={props.url}/> </div>
+            <div className="Preco"><h2>Preço:</h2><h2 id="h3valor">R${props.preco},00</h2></div>
+        </div>
+    )
+}
---
+++ frontend-master/src/data/produtos.js
@@ -0,0 +1,8 @@
+export default [
+    { id: 1, nome: 'Mesa', preco: 100, url:'https://static.carrefour.com.br/medias/sys_master/images/images/h79/h21/h00/h00/13388392071198.jpg' },
+    { id: 2, nome: 'HD', preco: 200, url:'https://img.terabyteshop.com.br/produto/g/hd-seagate-barracuda-st1000dm010-1tb-7200rpm-64mb-sata-iii_36317.jpg' },
+    { id: 3, nome: 'Mouse', preco: 300, url:'https://i.zst.com.br/images/mouse-optico-gamer-usb-g203-logitech-photo180801158-12-a-19.jpg' },
+    { id: 4, nome: 'Fonte', preco: 400, url:'https://www.miranda.com.br/lojas/2057/img/interno/prd_192662_p_39556.png' },
+    { id: 5, nome: 'Headset', preco: 500, url:'https://www.havan.com.br/media/catalog/product/cache/55f334c6f9412d6b39cfe195ce4e3943/h/e/headset-gamer-stereo-g332-logitech_280417_1.jpg' },
+    { id: 6, nome: 'Cadeira', preco: 600, url:'https://www.pichau.com.br/media/catalog/product/cache/4b8edbcff89db2e818b0fc2ad09d7f0d/b/y/by-8179-azulb821.jpg' },
+]
